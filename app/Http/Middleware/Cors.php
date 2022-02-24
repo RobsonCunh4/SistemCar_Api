30d1ce7249c2c8aa6141ed20dad60fdeb950be1a
StrongRC <95380035+StrongRC@users.noreply.github.com>
2022-02-24 00:20:21 -0300

    Update Cors.php

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -19,6 +19,7 @@ class Cors
     ->header("Access-Control-Allow-Origin", "*")
     ->header("Access-Control-Allow-Credentials", "true")
     ->header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-    ->header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, X-Token-Auth, Authorization");
+    ->header("Cache-Control: no-cache, must-revalidate")        
+    ->header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, X-Token-Auth, Authorization, Origin");      
   }
 }
