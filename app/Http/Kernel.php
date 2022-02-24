55f85fb65604c5296ed2d4a65a6e0353e9f5bb25
Robson Cunha <95380035+StrongRC@users.noreply.github.com>
2022-02-24 01:44:14 -0300

    Update Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -21,7 +21,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-        \App\Http\Middleware\CORS::class,
+        \App\Http\Middleware\Cors::class,
 
     ];
 
@@ -66,6 +66,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'jwt.auth' => \Tymon\JWTAuth\Http\Middleware\Authenticate::class,
-        'CORS' => \App\Http\Middleware\CORS::class,
+        'CORS' => \App\Http\Middleware\Cors::class,
     ];
 }
