4d6b5925ecd6d3caaeb04e2884c02bd16fda00d9
StrongRC <95380035+StrongRC@users.noreply.github.com>
2022-02-24 00:30:20 -0300

    Update Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -20,8 +20,7 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
-        \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-        \App\Http\Middleware\Cors::class
+        \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,        
     ];
 
     /**
@@ -64,7 +63,6 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'jwt.auth' => \Tymon\JWTAuth\Http\Middleware\Authenticate::class,
-        'cors' => \App\Http\Middleware\Cors::class
+        'jwt.auth' => \Tymon\JWTAuth\Http\Middleware\Authenticate::class        
     ];
 }
