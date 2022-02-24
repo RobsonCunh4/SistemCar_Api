dce6e2be00b93969034acefae1af88237754c78f
StrongRC <95380035+StrongRC@users.noreply.github.com>
2022-02-23 23:27:51 -0300

    Atualizado arquivo cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -5,17 +5,20 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 
-class cors
+class Cors 
 {
     /**
-     * Handle an incoming request.
+     * Get the path the user should be redirected to when they are not authenticated.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
-     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     * @return string|null
      */
-    public function handle(Request $request, Closure $next)
-    {
-        return $next($request);
-    }
+   public function handle(Request $request , Closure $next)
+  {
+    return $next($request)
+    //Acrescente as 3 linhas abaixo
+    ->header('Access-Control-Allow-Origin', "*")
+    ->header('Access-Control-Allow-Methods', "PUT, POST, DELETE, GET, OPTIONS")
+    ->header('Access-Control-Allow-Headers', "Accept, Authorization, Content-Type");
+  }
 }
