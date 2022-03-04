24d533dce419fd5c06e0ca8a4e8250f2bbeda75d
StrongRC <rcunha.sm@gmail.com>
2022-03-03 22:58:40 -0300

    ajuste cors novamente

--- config/cors.php
+++ config/cors.php
@@ -15,11 +15,11 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie', '*'],
+    'paths' => ['api/*', 'api/carro', 'api/cliente', 'sanctum/csrf-cookie', '*'],
 
-    'allowed_methods' => ['*'],
+    'allowed_methods' => ['POST', 'GET', 'DELETE', 'PUT', '*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://sistemcarfront.herokuapp.com', 'http://localhost:8080', '*'],
 
     'allowed_origins_patterns' => [],
 
